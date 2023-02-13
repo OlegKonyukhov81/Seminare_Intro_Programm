0266e697e4853361cc06da9a7a832b3eae3e083f
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-13 06:36:46 +0400

    Added_task01

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая проверяет является ли первое число квадратом второго.
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int square = numberB * numberB;
+if (numberA == square)
+{
+    Console.WriteLine($"Число {numberA} является квадратом {numberB}");
+}
+else
+{
+Console.WriteLine($"Число {numberA} не является квадратом {numberB}");
+}
