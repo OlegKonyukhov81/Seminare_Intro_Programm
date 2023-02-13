ddc42982dbdedefeb3d2d08f02f74322f36f737c
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-14 00:47:56 +0400

    Added_task02

---
+++ Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая на вход принимает 2 числа и выдает, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine($"max = {numberA}");
+}
+else 
+{
+    Console.WriteLine($"max = {numberB}");
+}
+if (numberA == numberB)
+{
+    Console.WriteLine("Числа равны");
+}
--- Task07/Program.cs
+++ Task07/Program.cs
@@ -5,6 +5,6 @@ int number = Convert.ToInt32(Console.ReadLine());
 if (number >= 100 && number <= 999)
 {
     int lastNumber = number % 10;
-    Console.Write($"Последняя цифра числа {number} является {lastNumber}"); 
+    Console.Write($"Последняя цифра числа {number} -> {lastNumber}"); 
 }
 else Console.Write("Вы ввели не трехзначное число");
