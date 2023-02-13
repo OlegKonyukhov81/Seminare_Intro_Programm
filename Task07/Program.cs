8379df1236141e75cdf42e30081f354a570aebd8
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-13 12:54:07 +0400

    Added_task07

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа, которая принимает на вход трехзначное число, 
+// а на выходе показывает последнюю цифру этого числа
+Console.Write("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int lastNumber = number % 10;
+    Console.Write($"Последняя цифра числа {number} является {lastNumber}"); 
+}
+else Console.Write("Вы ввели не трехзначное число");
