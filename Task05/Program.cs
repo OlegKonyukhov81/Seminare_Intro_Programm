8652f37803ec5139f80a71b31f4f46d8aab49239
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-13 10:05:21 +0400

    Added_task05

---
+++ Task05/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая на вход принимает число N
+// на выходе показывает все целые числа в промежутке от -N до N
+// 4 -> -4 -3 -2 -1 0 1 2 3 4
+Console.Write("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = -number;
+while (index <= number)
+{
+    Console.Write(index + " ");
+    index ++;
+}
