35d9bd7019b73bde899b7bb8862ad98bcb87f499
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-14 15:44:58 +0400

    Added_Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая на вход принимает число и выдает, является ли это число четным
+// (делится ли оно на два без остатка)
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int evenNumber = number % 2;
+if (number != 0)
+{
+    if (evenNumber != 0) 
+    Console.WriteLine($"Число {number} - нечетное");
+    else 
+    Console.WriteLine($"Число {number} - четное");
+}
+else 
+Console.WriteLine($"Вы ввели нуль");
