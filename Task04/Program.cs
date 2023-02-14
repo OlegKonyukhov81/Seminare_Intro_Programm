ddd36a06502217c409786349b0576acbe2e70856
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-14 14:52:35 +0400

    Added_Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа, которая на вход принимает три числа, а выдает максимальное из этих чисел
+// 
+Console.WriteLine("Введите число 1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int MaxNumber = numberA;
+if (numberA > numberB) 
+{
+    if (numberC > numberA) 
+    {
+    Console.Write($"Максимум -> {numberC}");
+    }
+    else
+    Console.Write($"Максимум -> {numberA}");
+}
+else
+{
+    if (numberC > numberB)
+    Console.Write($"Max -> {numberC}");
+    else
+    Console.Write($"Max -> {numberB}");
+}
