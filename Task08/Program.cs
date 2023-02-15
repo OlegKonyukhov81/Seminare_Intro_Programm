f39de8cd135a6513a7d2a44bc1c3dd86410556f3
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-15 09:29:15 +0400

    Added_Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая на вход принимает число (N),
+// а на выходе показывает все четные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+while (index <= number)
+{
+    Console.Write(index + ", ");
+    index = index + 2;
+}
