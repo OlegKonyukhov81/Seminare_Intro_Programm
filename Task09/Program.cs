15fc5b839b10d594a4dd810689c761f9dbb8ed35
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-17 13:52:55 +0400

    Draft_Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа, которая выводит случайное число от10до99 и показывает наибольшую цифру этого числа.
+
+int number = new Random().Next(10, 100);
+// Console.WriteLine(number);
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (firstDigit > secondDigit)
+// {
+//     Console.WriteLine($"Наибольшая цифра числа {firstDigit}");
+// }
+// else 
+// Console.WriteLine($"Наибольшая цифра числа {secondDigit}");
+
+
+int MaxDigit(int num)
+{
+    int max = num;
+    int firDig = number / 10;
+    int secDig = number % 10;
+    if ()
+}
+
+int maximal = MaxDigit(int num);
