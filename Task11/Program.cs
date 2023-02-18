00660aa8ec6d0e6fa8b961617c2527b849d6e7c6
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-18 09:43:56 +0400

    Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая выводит случайное трехзначное число, затем удаляет вторую цифру этого числа.
+
+int thirdNum = new Random().Next(100, 999);
+Console.WriteLine($"Случайное трехзначное число - {thirdNum}");
+
+int DeleteSecondDigit(int num)
+{
+    int num1 = num / 100;
+    int num2 = num % 10;
+    return num1 * 10 + num2;
+}
+
+int withOutTwoNum = DeleteSecondDigit(thirdNum);
+Console.WriteLine($"Число без второй цифры - {withOutTwoNum}");
