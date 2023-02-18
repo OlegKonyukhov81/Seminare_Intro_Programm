8ac1f5ce75495a115f9939254590d64a15b93d62
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-18 16:59:51 +0400

    Task16_Check_Square_Numbers

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -6,19 +6,20 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число - ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
+// if (CheckSquareNumbers(number1, number2)) Console.WriteLine($"{number1}, {number2} -> ДА");
+// else Console.WriteLine($"{number1}, {number2} -> НЕТ");
+
 // bool CheckSquareNumbers(int firstNumber, int secondNumber)
 // {
 //     return firstNumber * firstNumber == secondNumber || secondNumber * secondNumber == firstNumber;
 // }
 
-// if (CheckSquareNumbers(number1, number2)) Console.WriteLine($"{number1}, {number2} -> ДА");
-// else Console.WriteLine($"{number1}, {number2} -> НЕТ");
 
 void CheckSquareNumbers(int num1, int num2)
 {
     if (num1 * num1 == num2 || num2 * num2 == num1)
     Console.WriteLine($"{num1}, {num2} -> ДА");
     else
-    Console.WriteLine($"{num1}, {num1} -> НЕТ");
+    Console.WriteLine($"{num1}, {num2} -> НЕТ");
 }
  CheckSquareNumbers(number1, number2);
