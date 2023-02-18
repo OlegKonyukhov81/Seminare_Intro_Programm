5f1ef01f279d8d985770723ab424b07fa6cb36bf
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-18 14:18:20 +0400

    Multiplicity_7_and_23_With_boolMetod

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -3,11 +3,19 @@
 Console.WriteLine("Введите число - ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-void CheckMultiplicity7And23(int number)
+// void CheckMultiplicity7And23(int number)
+// {
+//     if (number % 7 == 0 && number % 23 == 0)
+//     Console.WriteLine($"{number} -> Да");
+//     else Console.WriteLine($"{number} -> Нет");
+// }
+
+// CheckMultiplicity7And23(num);
+
+bool CheckMultiplicity7and23(int number)
 {
-    if (number % 7 == 0 && number % 23 == 0)
-    Console.WriteLine($"{number} -> Да");
-    else Console.WriteLine($"{number} -> Нет");
+    return number % 7 == 0 && number % 23 == 0;
 }
 
-CheckMultiplicity7And23(num);
+if (CheckMultiplicity7and23(num)) Console.WriteLine($"{num} -> ДА");
+else Console.WriteLine($"{num} -> НЕТ");
