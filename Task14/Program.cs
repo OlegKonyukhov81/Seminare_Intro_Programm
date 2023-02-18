33f4bdab606ef8334516e6b153fe07206aae5d50
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-18 13:24:35 +0400

    Multiplicity_7_and_23

---
+++ Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает число и проверяет кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число - ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void CheckMultiplicity7And23(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0)
+    Console.WriteLine($"{number} -> Да");
+    else Console.WriteLine($"{number} -> Нет");
+}
+
+CheckMultiplicity7And23(num);
