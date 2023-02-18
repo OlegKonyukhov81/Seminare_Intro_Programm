10ff4a822c238fb98e092a5421a0c1a521fc3ba4
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-18 18:44:16 +0400

    Task10_PullSecondDigit_FromThreeDigit

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая на вход принимает трехзначное число, а выдает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число -");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void PullSecondDigit(int threeDigitNum)
+{
+    if (threeDigitNum / 1000 == 0 && threeDigitNum / 100 >= 1)
+    {
+        int secondDigit = (threeDigitNum % 100) / 10;
+        Console.WriteLine($"{threeDigitNum} -> {secondDigit}");
+    }
+    else Console.WriteLine($"Вы ввели не трехзначное число");
+}
+PullSecondDigit(number);
+
+
+
