9da69f440bada8b79eec1f7c76748823648c3165
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-19 10:35:29 +0400

    Task13_With_Module_Version

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,11 +1,22 @@
 ﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что этой цифры нет.
 
-Console.Write("Введите целое число - ");
+Console.Write("Введите целое число : ");
 int currentNumber = Convert.ToInt32(Console.ReadLine());
 
 void PullThirdDigit(int number)
 {
-    if (number / 100 >= 1)
+    if (number < 0)
+    {
+        int moduleNumber = number * (-1);
+        if (moduleNumber / 100 >= 1)
+        {
+            int thirdDigit1 = (moduleNumber / 100) % 10;
+            Console.WriteLine($"{number} -> {thirdDigit1}");
+        }
+        else Console.WriteLine($"{number} -> третьей цифры нет");
+    }
+
+    else if (number / 100 >= 1)
     {
         int thirdDigit = (number / 100) % 10;
         Console.WriteLine($"{number} -> {thirdDigit}");
