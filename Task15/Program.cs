ebe03478f22a2be5830e3cecb9c23fc0a1ea3c33
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-19 12:16:44 +0400

    Task15_Search_WeekEnd

---
+++ Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая принимает номер дня недели и проверяет является ли этот день выходным.
+
+Console.Write("Введите номер дня недели - ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void SearchWeekend(int day)
+{
+    if (day >= 1 && day <= 7)
+    {
+        if (day == 6 || day == 7)
+        {
+            Console.WriteLine($"{day} -> Да");
+        }
+        else Console.WriteLine($"{day} -> Нет");
+    }
+    else Console.WriteLine("Введите корректный день недели");
+}
+
+SearchWeekend(dayNumber);
