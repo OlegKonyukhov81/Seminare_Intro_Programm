e0900e70800d94c98d403613957dd1af41fa5b94
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-19 09:57:10 +0400

    Task10_With_Module_Version

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,11 +1,21 @@
 ﻿// Программа, которая на вход принимает трехзначное число, а выдает вторую цифру этого числа.
 
-Console.WriteLine("Введите трехзначное число -");
+Console.Write("Введите трехзначное число : ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 void PullSecondDigit(int threeDigitNum)
 {
-    if (threeDigitNum / 1000 == 0 && threeDigitNum / 100 >= 1)
+    if (threeDigitNum < 0)
+    {
+        int moduleNumber = threeDigitNum * (-1);
+        if (moduleNumber / 1000 <= 0 && moduleNumber / 100 >= 1)
+        {
+            int secondDigit1 = (moduleNumber % 100) / 10;
+            Console.WriteLine($"{threeDigitNum} -> {secondDigit1}");
+        }
+        else Console.WriteLine($"Вы ввели не трехзначное число");
+    }
+    else if (threeDigitNum / 1000 <= 0 && threeDigitNum / 100 >= 1)
     {
         int secondDigit = (threeDigitNum % 100) / 10;
         Console.WriteLine($"{threeDigitNum} -> {secondDigit}");
---
+++ Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что этой цифры нет.
+
+Console.Write("Введите целое число - ");
+int currentNumber = Convert.ToInt32(Console.ReadLine());
+
+void PullThirdDigit(int number)
+{
+    if (number / 100 >= 1)
+    {
+        int thirdDigit = (number / 100) % 10;
+        Console.WriteLine($"{number} -> {thirdDigit}");
+    }
+    else Console.WriteLine($"{number} -> теретьей цифры нет");
+}
+PullThirdDigit(currentNumber);
