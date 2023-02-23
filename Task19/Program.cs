8c84ccdf6ae17773688a5a00de0477f2d6fe41f3
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-24 01:11:43 +0400

    Check_Palindrome

---
+++ Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа, которая принимает 5-ти значное число и проверяет, является ли оно полиндромом
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool ValidateNumber(int fiveDiditNum)
+{
+    return fiveDiditNum >= 10000 && fiveDiditNum <= 99999;
+}
+
+void CheckPalindrome(int number)
+{
+    if (number / 10000 == number % 10)
+    {
+        if (number / 1000 % 10 == number / 10 % 10)
+        Console.WriteLine($"{number} -> Да");
+    }
+    else Console.WriteLine($"{number} -> Нет");
+}
+if (ValidateNumber(num)) 
+{
+    CheckPalindrome(num);
+}
+else Console.WriteLine("Вы ввели не пятизначное число!");
+
+
