3923c8ba9ca412738bd77c38a495f87b620d27ce
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-23 21:10:46 +0400

    Search_Coordinates

---
+++ Task18/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая по заданному номеру четверти выдает возможный диапазон координат в этой четверти.
+
+Console.Write("Введите номер четверти от 1 до 4: ");
+int numberQuarter = Convert.ToInt32(Console.ReadLine());
+
+bool ValidateQuarter(int quarter)
+{
+    return quarter > 0 && quarter < 5;
+}
+
+void SearchCoordinates(int quarter)
+{
+    if (quarter == 1) Console.WriteLine("x > 0; y > 0");
+    if (quarter == 2) Console.WriteLine("x < 0; y > 0");
+    if (quarter == 3) Console.WriteLine("x < 0; y < 0");
+    if (quarter == 4) Console.WriteLine("x > 0; y < 0");
+}
+
+if (ValidateQuarter(numberQuarter))
+SearchCoordinates(numberQuarter);
+else Console.WriteLine("Введите корректный номер четверти!");
