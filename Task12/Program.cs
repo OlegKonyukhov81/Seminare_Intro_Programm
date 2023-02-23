12dda39713bfeb8c17de888297fc2136de0924a8
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-23 16:19:22 +0400

    Search_Quarter

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,20 +1,41 @@
 ﻿// Программа, которая на вход принимает два числа, а выводит - является ли первое число кратным второму,
 // Если 1 число не кратно 2-му, программа выводит остаток.
 
-Console.WriteLine("Введите первое число - ");
+Console.Write("Введите первое число : ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число - ");
+Console.Write("Введите второе число : ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-void CheckMultipliDig(int firstNumber, int secondNumber)
+// void CheckMultipliDig(int firstNumber, int secondNumber)
+// {
+//     int remainder = firstNumber % secondNumber; // переменная remainder - остаток от деления num1 на num2
+//     if (remainder == 0)
+//     {
+//         Console.WriteLine($"Число {firstNumber} кратно {secondNumber}");
+//     }
+//     else Console.WriteLine($"Остаток {remainder}");
+// }
+
+// CheckMultipliDig(num1, num2);
+
+bool CheckMultipliDigits(int firstNumber, int secondNumber)
 {
-    int remainder = firstNumber % secondNumber; // переменная remainder - остаток от деления num1 на num2
-    if (remainder == 0)
+    if (firstNumber % secondNumber == 0)
     {
-        Console.WriteLine($"Число {firstNumber} кратно {secondNumber}");
+        return true;
     }
-    else Console.WriteLine($"Остаток {remainder}");
+    return false;
 }
 
-CheckMultipliDig(num1, num2);
+int CheckReminder(int firstNumber, int secondNumber)
+{
+    return firstNumber % secondNumber;
+}
+
+if (CheckMultipliDigits(num1, num2))
+{
+    Console.WriteLine($"{num1} кратно {num2}");
+}
+int reminder = CheckReminder(num1, num2);
+Console.WriteLine(reminder);
---
+++ Task17/Program.cs
@@ -0,0 +1,35 @@
+﻿// Программа, которая на вход принимает координаты (х, у)
+// причем х,у не равны 0, а выдает номер четверти.
+
+Console.Write("Введите координату Х точки: ");
+int coordinateX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату У точки: ");
+int coordinateY = Convert.ToInt32(Console.ReadLine());
+
+int SearchQuarter(int absciss, int ordinate)
+{
+        if (absciss > 0 && ordinate > 0) return 1;
+        if (absciss > 0 && ordinate < 0) return 4;
+        if (absciss < 0 && ordinate > 0) return 2;
+        if (absciss < 0 && ordinate < 0) return 3;
+        return 0;
+}
+// int quarter = SearchQuarter(coordinateX, coordinateY);
+// if (quarter == 0) Console.WriteLine("Вы ввели ноль!");
+// else Console.WriteLine($"{quarter}");
+
+// или
+
+int quarter = SearchQuarter(coordinateX, coordinateY);
+string result = quarter > 0 ? $"{quarter}" : "Вы ввели ноль!";
+Console.WriteLine(result);
+
+
+// int Prompt(string message);
+// {
+//     Console.WriteLine(message); 
+//     string value = Console.ReadLine();
+//     int result = Convert.ToInt32(value);
+//     return result;
+// }
+// int number = Prompt("Введите ")
