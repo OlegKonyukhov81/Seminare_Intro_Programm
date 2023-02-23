2dafca35d72c5b9110b12eb4d22ac68d6a51b678
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-24 00:16:06 +0400

    Table_Square_Numbers

---
+++ Task22/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая на вход принимает число N а выдает таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Square(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index, 5} - {index * index, 5}");
+        index ++;
+    }
+}
+Square(number);
+
+
