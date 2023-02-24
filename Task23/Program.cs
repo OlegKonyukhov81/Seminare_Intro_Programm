99a88349dac457a91d9f5313bd6af84c38175cf7
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-24 06:39:08 +0400

    Table_Cube_Numbers

---
+++ Task23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая на вход принимает число N а выдает таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void CubeNum(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index, 6} - {index * index * index, 6}");
+        index ++;
+    }
+}
+CubeNum(number);
