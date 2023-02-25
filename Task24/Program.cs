37fb7fe2ea13e3226201b6d33a21c7ba57b2b0a6
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-25 14:19:42 +0400

    Task24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -10,35 +10,35 @@
 Console.Write("Введите положительное целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-// bool ValidatePositiveNum(int value)
-// {
-//     if (value >= 0 && value % 1 == 0) return true;
-//     else return false;
-// }
+bool ValidatePositiveNum(int value)
+{
+    if (value >= 0 && value % 1 == 0) return true;
+    else return false;
+}
 
 // ИЛИ
 
-int SumNumbers(int lastNumber)
-{
-    if (lastNumber >= 0 && lastNumber % 1 == 0)
-    {
-        int currentNum = 0;
-        int sumNumber = 0;
-        while (currentNum <= lastNumber)
-        {
-            sumNumber = sumNumber + currentNum;
-            currentNum +=1;
-        }
-        return sumNumber;
-    }
-    else return -1;
-}
-int sumNum = SumNumbers(number);
-if (sumNum > 0)
-{
-    Console.WriteLine($"{number} -> {sumNum}");
-}
-else Console.WriteLine("Вы ввели отрицательное число!");
+// int SumNumbers(int lastNumber)
+// {
+//     if (lastNumber >= 0 && lastNumber % 1 == 0)
+//     {
+//         int currentNum = 0;
+//         int sumNumber = 0;
+//         while (currentNum <= lastNumber)
+//         {
+//             sumNumber = sumNumber + currentNum;
+//             currentNum +=1;
+//         }
+//         return sumNumber;
+//     }
+//     else return -1;
+// }
+// int sumNum = SumNumbers(number);
+// if (sumNum > 0)
+// {
+//     Console.WriteLine($"{number} -> {sumNum}");
+// }
+// else Console.WriteLine("Вы ввели отрицательное число!");
 
 // ИЛИ
 
@@ -59,3 +59,22 @@ else Console.WriteLine("Вы ввели отрицательное число!")
 //     SumNumbers(number);
 // }
 // else Console.WriteLine("Вы ввели отрицательное число!");
+
+// ИЛИ
+
+int SumNumbers(int lastNumber)
+{
+        int sumNumber = 0;
+        for (int i=0; i <= lastNumber; i ++)
+        {
+            sumNumber = sumNumber + i;
+        }
+        return sumNumber;
+}
+
+if (ValidatePositiveNum(number))
+{
+    int sumNum = SumNumbers(number); 
+    Console.WriteLine($"{number} -> {sumNum}");
+}
+else Console.WriteLine("Вы ввели отрицательное число!");
