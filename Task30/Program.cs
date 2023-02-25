4ce885737847ba35d6ad2814d000fbabdfc91ad0
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-25 22:05:38 +0400

    Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа, которая выводит массив из 8 элементов, заполненный 1 и 0 в случайном порядке.
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 2);
+        index +=1;
+    }
+}
+void PrintArray(int[] generated)
+{
+    int count = generated.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(generated[position]);
+        position +=1;
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
