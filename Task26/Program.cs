7263b124739af10c5d94a92dfb7e2b46a3d54e2a
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-25 17:36:29 +0400

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа, которая на вход принимает число, а выдает количество цифр в этом числе.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool ValidateZero(int value)
+{
+    if (value != 0) return true;
+    else return false;
+}
+
+int CountDigits(int exploreNumber)
+{
+    int countDigits = 0;
+    int cutNumber = 10;
+    int currentNumber = exploreNumber;
+    while (currentNumber > 0 || currentNumber % 10 > 0)
+    {
+        countDigits += 1;
+        currentNumber = currentNumber / 10;
+        cutNumber = cutNumber * 10;
+    }
+    return countDigits;
+}
+
+if (ValidateZero(number))
+{
+    int count = CountDigits(number);
+    Console.WriteLine($"{number} -> {count}");
+}
+else Console.WriteLine($"{number} -> 1");
