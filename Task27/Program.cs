d4606239e6b948ad02e354ce5fe2d01d6f06b59a
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-26 19:02:58 +0400

    Task27_SumDigitsOfNumber

---
+++ Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая на вход принимает число, а на выходе дает сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigitsOfNumber(int exploreNum)
+{
+    int currentNum = Math.Abs(exploreNum);
+    int sumNumber = 0;
+    while (currentNum > 0)
+    {
+        sumNumber = sumNumber + (currentNum % 10);
+        currentNum = currentNum / 10;
+    }
+    return sumNumber;
+}
+int sumDigits = SumDigitsOfNumber(number);
+Console.WriteLine(sumDigits);
