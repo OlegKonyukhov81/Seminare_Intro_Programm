fc843cc86a4a4bb51bcd9462942fdcebcaa8a1eb
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-02-27 16:48:54 +0400

    Task31_Sum_Positiv_&_NegativeNumbers

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,36 +1,73 @@
 ﻿// Программа, которая задает массив из 8 элементов, заполненный псевдослучайными числами и выводит их на
 // экран.
 
-void FillArray(int[] generated)
+// void FillArray(int[] generated)
+// {
+//     int length = generated.Length;
+//     int index = 0;
+//     Random rndm = new Random();
+//     while (index < length)
+//     {
+//         generated[index] = rndm.Next(0, 100);
+//         index +=1;       
+//     }
+// }
+// void PrintArray(int[] collection)
+// {
+//     int count = collection.Length;
+//     int position = 0;
+//     // Console.Write("["); лучше выводить без скобок, так будет универсальней, 
+//     while (position < count)        // а скобки добавить при вызове метода. См. вызов.
+//     {
+//         Console.Write($"{collection[position]}");
+//         if (position < count-1) Console.Write(", ");
+//         position +=1;
+//     }
+// }
+
+//     // Console.Write("]");
+
+// int[] array = new int[8];
+// FillArray(array);
+// PrintArray(array);
+// Console.Write(" -> ");
+// Console.Write("[");
+// PrintArray(array);
+// Console.Write("]");
+
+// То же самое, только пользователь вводит количество элементов и диапазон случайных чисел.
+
+Console.Write("Введите количество элементов в массиве: ");
+int volume = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное случайное число массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное случайное число массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] array = ArrayNumbers(volume, min, max);
+PrintArrayNumbers(array);
+
+int[] ArrayNumbers(int num1, int num2, int num3)
 {
-    int length = generated.Length;
-    int index = 0;
     Random rndm = new Random();
-    while (index < length)
+    int[] random = new int[num1];
+    for (int i = 0; i < num1; i ++)
     {
-        generated[index] = rndm.Next(0, 100);
-        index +=1;       
+        random[i] = rndm.Next(num2, num3 + 1);
     }
+    return random;
 }
-void PrintArray(int[] collection)
+
+void PrintArrayNumbers(int[] arrayNumbers)
 {
-    int count = collection.Length;
-    int position = 0;
-    // Console.Write("["); лучше выводить без скобок, так будет универсальней, 
-    while (position < count)        // а скобки добавить при вызове метода. См. вызов.
+    int length = arrayNumbers.Length;
+    int count = 0;
+    while (count < length)
+    // for (int i = 0; i < arrayNumbers.Length; i ++)
     {
-        Console.Write($"{collection[position]}");
-        if (position < count-1) Console.Write(", ");
-        position +=1;
+        // Console.Write(arrayNumbers[i]);
+        // if (i < arrayNumbers.Length - 1) Console.Write(", ");
+        Console.Write(arrayNumbers[count]);
+        if (count < length - 1) Console.Write(", ");
+        count ++;
     }
 }
-
-    // Console.Write("]");
-
-int[] array = new int[8];
-FillArray(array);
-PrintArray(array);
-Console.Write(" -> ");
-Console.Write("[");
-PrintArray(array);
-Console.Write("]");
--- Task31/Program.cs
+++ Task31/Program.cs
@@ -22,27 +22,74 @@ void PrintArray(int[] arr)
     for (int i = 0; i < arr.Length; i ++)
     {
         if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
-        else Console.Write($"{arr[i]},  ");
+        else Console.Write($"{arr[i]}");
     }
     Console.Write("]");
-    return arr;
-} 
-int[] array = CreateArrayRndInt(12, -9, 9);
-PrintArray(array);
-
-int[] GetSumPositiveNegativeElem()
-{
-    int sumNegative = 0;
-    int sumPositive = 0;
-    for (int i = 0; i < arr.Length; i ++)
-    {
-        if (arr[i] < 0) sumNegative += arr[i];
-        else sumPositive += arr[i];
-    }
-    return new int[]{sumNegative, sumPositive};
 }
 int[] array = CreateArrayRndInt(12, -9, 9);
 PrintArray(array);
-int[] sumPositiveNegativeElem = GetSumPositiveNegativeElem(array);
-Console.WriteLine($"Сумма положительных чисел = {sumPositiveNegativeElem[1]}");
-Console.WriteLine($"Сумма отрицательных чисел = {sumPositiveNegativeElem[0]}");
+// создаем новую переменную(массив)- результат работы метода SumPositiveNegativeElem(array)
+int[] devidedArray = SumPositiveNegativeElem(array);
+Console.WriteLine(); // разделительная пустая полоса
+Console.WriteLine($"Сумма положительных чисел = {devidedArray[0]}");
+Console.WriteLine($"Сумма отрицательных чисел = {devidedArray[1]}");
+
+
+// SumPositiveNegativeElem(array); (вызов void)
+
+// Метод void
+
+// void SumPositiveNegativeElem(int[] randomArr)
+// {
+//     int sumPositiveNum = 0;
+//     int sumNegativeNum = 0;
+//     for (int i = 0; i < randomArr.Length; i ++)
+//     {
+//         if (randomArr[i] >= 0) sumPositiveNum += randomArr[i];
+//         else sumNegativeNum += randomArr[i];
+//     }
+//     Console.WriteLine();
+//     Console.WriteLine($"Сумма положительных = {sumPositiveNum}");
+//     Console.WriteLine($"Сумма отрицательных = {sumNegativeNum}");
+// }
+
+// Метод int (возвращаемый)
+
+int[] SumPositiveNegativeElem(int[] randomArr)
+{
+    int sumPositiveNum = 0;
+    int sumNegativeNum = 0;
+    for (int i = 0; i < randomArr.Length; i ++)
+    {
+        if (randomArr[i] >= 0) sumPositiveNum += randomArr[i];
+        else sumNegativeNum += randomArr[i];
+    }
+    return new int[]{sumPositiveNum, sumNegativeNum};
+}
+
+
+
+
+
+
+
+
+
+
+
+// int[] GetSumPositiveNegativeElem()
+// {
+//     int sumNegative = 0;
+//     int sumPositive = 0;
+//     for (int i = 0; i < arr.Length; i ++)
+//     {
+//         if (arr[i] < 0) sumNegative += arr[i];
+//         else sumPositive += arr[i];
+//     }
+//     return new int[]{sumNegative, sumPositive};
+// }
+// int[] array = CreateArrayRndInt(12, -9, 9);
+// PrintArray(array);
+// int[] sumPositiveNegativeElem = GetSumPositiveNegativeElem(array);
+// Console.WriteLine($"Сумма положительных чисел = {sumPositiveNegativeElem[1]}");
+// Console.WriteLine($"Сумма отрицательных чисел = {sumPositiveNegativeElem[0]}");
