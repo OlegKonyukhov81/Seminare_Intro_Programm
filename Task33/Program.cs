619690bc05a10775a0af644741860797332e4463
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-03-01 11:57:37 +0400

    Task33_SearchNum_And_YesOrNo

--- Task33/Program.cs
+++ Task33/Program.cs
@@ -3,12 +3,14 @@
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // 3; массив [6, 7, 19, 345, 3] -> да
 
-Console.Write("Введите количество элементов в массиве: ");
-int volume = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите минимальное случайное число массива: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите максимальное случайное число массива: ");
-int max = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество элементов в массиве: ");
+// int volume = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите минимальное случайное число массива: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимальное случайное число массива: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите искомое число: ");
+int searchNum = Convert.ToInt32(Console.ReadLine());
 
 
 int[] RandomArray(int num1, int num2, int num3)
@@ -30,9 +32,45 @@ void PrintRandomArray(int[] printArray)
         Console.Write($"{printArray[i]}");
         if (i < printArray.Length -1) Console.Write(", ");
     }
-    Console.Write("[");
+    Console.Write("]");
 }
 
-int[] freeArray = RandomArray(volume, min, max);
+// int PositiveFind(int[] explore, int searchableNum)
+// {
+//     for (int i = 0; i < explore.Length; i ++)
+//     {
+//         if (explore[i] == searchableNum)
+//         {
+//             return searchableNum;
+//         }
+//         else break;
+//     }
+
+// }
+void NegativeFind(int[] explore, int searchableNum)
+{
+    int cloudNum = 1;
+    for (int i = 0; i < explore.Length; i ++)
+    {
+        if (explore[i] == searchableNum)
+        {
+            Console.Write(" -> Да ");
+            break;
+        }
+        else
+        {
+            cloudNum = cloudNum + 1;
+        }
+        if (cloudNum == explore.Length - 1)
+    {
+        Console.Write(" -> Нет ");
+    }
+    }
+}
+
+Console.Write($"{searchNum}; массив ");
+int[] freeArray = RandomArray(10, -10, 10);
 
 PrintRandomArray(freeArray);
+// PositiveFind(freeArray, searchNum);
+NegativeFind(freeArray, searchNum);
