69dca59bf48143c21a456082db42643f209b8461
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-03-02 16:16:43 +0400

    Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Необходимо задать массив случайных чисел и показать количество элементов стоящих на нечетных позициях.
+// [.......] -> 9
+
+int[] RandomArray(int num1, int num2, int num3)
+{
+    int[] randomArray = new int[num1];
+    Random rndm = new Random();
+    for (int i = 0; i < num1; i ++)
+    {
+        randomArray[i] = rndm.Next(num2, num3 + 1);
+    }
+    return randomArray;
+}
+
+void PrintRandomArray(int[] printArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < printArray.Length; i ++)
+    {
+        Console.Write($"{printArray[i]}");
+        if (i < printArray.Length -1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+int EvenPositionSummNum(int[] inputArray)
+{
+    int summNumbers = 0;
+    for (int i = 0; i < inputArray.Length; i +=2)
+    {
+        summNumbers = summNumbers + inputArray[i];
+    }
+    return summNumbers;
+}
+
+int [] randomArray = RandomArray(10, -20, 20);
+PrintRandomArray(randomArray);
+int evenPositionSummNum = EvenPositionSummNum(randomArray);
+Console.Write($" -> {evenPositionSummNum}");
