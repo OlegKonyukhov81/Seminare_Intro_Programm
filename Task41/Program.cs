c7ddde37172d276afce68e7b22671a636c2de00f
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-03-05 13:22:01 +0400

    Task42_InputArrayFromConsole

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,4 +1,60 @@
 ﻿// Программа, которая преобразовывает десятичное число в двоичное.
+// 45 -> 101101
 
-Console.WriteLine("Hello, World!");
 
+int NumberLength(int exploreNum)
+{
+    int count = 0;
+    int workNum = exploreNum;
+    while (workNum != 0)
+    {
+        workNum = workNum / 2;
+        count +=1;
+    }
+    return count;
+}
+
+int[] ConvertInBinarySystem(int lengthArray, int exploreNum)
+{
+    int[] convertInBinary = new int[lengthArray];
+    // int currentNum = exploreNum;
+    for (int i = lengthArray - 1; i >= 0; i -=1)
+    {
+        convertInBinary[i] = exploreNum % 2;
+        exploreNum = exploreNum / 2;
+    }
+    return convertInBinary;
+}
+
+void PrintRandomArray(int[] printArray)
+{
+    for (int i = 0; i < printArray.Length; i ++)
+    {
+        Console.Write($"{printArray[i]}");
+    }
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int numberLength = NumberLength(number);
+// // Console.Write($"{numberLength}");
+// int[] binaryNumber = ConvertInBinarySystem(numberLength, number);
+// Console.Write($"{number} -> ");
+// PrintRandomArray(binaryNumber);
+
+// или через строку
+
+// string ConvertInBinarySystem(int num)
+// {
+//     string numberBinary = string.Empty;
+//     while (num > 0)
+//     {
+//         numberBinary = num % 2 + numberBinary; // от перемены мест слагаемых, 
+//         num = num / 2;                         // меняется их расположение в рельтате(ответе).
+//     }
+//     return numberBinary;
+// }
+
+// string convertInBinary = ConvertInBinarySystem(number);
+// Console.Write(convertInBinary);
