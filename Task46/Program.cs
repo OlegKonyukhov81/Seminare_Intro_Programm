d25b393aefd668e9f964e03906bafbf987010469
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-03-11 08:19:29 +0400

    Task48_Fill_MatrixArray_Numbers

---
+++ Task46/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив m x n, заполненный случайными целыми числами. m=3, n=4.
+
+int[,] RandomArray(int rows, int colomns, int min, int max)
+{
+    int[,] matrix = new int[rows, colomns];
+    Random rndm = new Random();
+    for (int i = 0; i < rows; i ++) // или < matrix.GetLength(0)
+    {
+        for (int j = 0; j < colomns; j ++)// или < matrix.GetLength(1)
+        matrix[i, j] = rndm.Next(min, max + 1);
+    }
+    return matrix;
+}
+
+void PrintRandomArray(int[,] printArray)
+{
+    for (int i = 0; i < printArray.GetLength(0); i ++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < printArray.GetLength(1); j++)
+        {
+            Console.Write($"{printArray[i, j], 3}");
+        }
+    }
+}
+
+int[,] matrixArray = RandomArray(3, 4, -50, 50);
+PrintRandomArray(matrixArray);
---
+++ Task48/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задать двумерный массив размера m x n, каждый элемент в массиве находится по формуле Аmn = m + n
+// Вывести полученный массив на экран.
+// 0  1  2  3
+// 1  2  3  4
+// 2  3  4  5
+
+int[,] Matrix(int rows, int colomns)
+{
+    int[,] matrix = new int[rows, colomns];
+    for (int i = 0; i < rows; i ++) // или < matrix.GetLength(0)
+    {
+        for (int j = 0; j < colomns; j ++)// или < matrix.GetLength(1)
+        matrix[i, j] = i + j;
+    }
+    return matrix;
+}
+
+void PrintRandomArray(int[,] printArray)
+{
+    for (int i = 0; i < printArray.GetLength(0); i ++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < printArray.GetLength(1); j++)
+        {
+            Console.Write($"{printArray[i, j], 3}");
+        }
+    }
+}
+
+int[,] matrixArray = Matrix(3, 4);
+PrintRandomArray(matrixArray);
