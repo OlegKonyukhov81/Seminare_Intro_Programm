a4a5e111b2b38053fe4417a23978fe666683fe90
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-03-23 22:23:43 +0400

    Task60_Edit_Print3D_Matrix

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -5,10 +5,18 @@
 // 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
 // 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
 
+Console.WriteLine("Введите количество строк: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество аппликат: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
 int[,,] Matrix3D(int rows, int colomns, int depth, int min, int max)
 {
     int[,,] matrix = new int[rows, colomns, depth];
     int currentNum = min;
+
     for (int i = 0; i < rows; i ++) 
     {
         for (int j = 0; j < colomns; j ++)
@@ -38,5 +46,17 @@ void PrintMatrix3D(int[,,] printMatrix)
     }
 }
 
-int [,,] matrix3D = Matrix3D(2, 2, 2, 10, 100);
-PrintMatrix3D(matrix3D);
+bool ValidateMatrix3D(int rows, int colomns, int depth)
+{
+    int volumeMatrix = 89;
+    if (rows * colomns * depth < volumeMatrix)
+    return true;
+    else return false;
+}
+
+if (ValidateMatrix3D(num1, num2, num3))
+{
+    int [,,] matrix3D = Matrix3D(num1, num2, num3, 10, 100);
+    PrintMatrix3D(matrix3D);
+}
+else Console.WriteLine("Невозможно создать из неповторяющихся двухзначных чисел!");
