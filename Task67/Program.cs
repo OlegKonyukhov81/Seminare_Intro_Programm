4f001e8a4e859ca90a0bb7fa30d1a591f8550fc9
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-03-25 05:07:36 +0400

    Task67_Recursion_SumDigits

---
+++ Task67/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumDigitsNumber(int number)
+{
+    if (number == 0) return 0;
+    else return number % 10 + SumDigitsNumber(number / 10);
+    Console.WriteLine($"{number}");
+}
+
+int sumDigits = SumDigitsNumber(num);
+Console.WriteLine(sumDigits);
---
+++ Task69/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 ->
+
+Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Power(int number, int degree)
+{
+    if (degree == 0) return 1;
+    else return number * Power(number, degree - 1);
+}
+Console.WriteLine(Power(num1, num2));
